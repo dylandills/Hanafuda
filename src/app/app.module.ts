36134f385e5cc8c8a616d43b8cc2b0e9034a8e45
eluts <eluts15@gmail.com>
2017-07-20 11:16:52 -0700

    add card-detail component, add service to converse between playfield and card-detail components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PlayfieldComponent } from './playfield/playfield.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayfieldComponent,
+    CardDetailComponent
   ],
   imports: [
     BrowserModule,
