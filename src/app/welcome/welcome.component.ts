0bd9fa6a1e444e59c062bf585e3852828fd51d0a
Anabel Ramirez and Ethan Luts <anabel+ethan@>
2017-07-20 10:44:46 -0700

    add player.service, begin welcome component with user input fields.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../player.service';
+import { Player } from '../player.model';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.sass']
+  styleUrls: ['./welcome.component.sass'],
+  providers: [PlayerService]
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
   }
 
+  submitForm(name: string, birthMonth: string, score: number) {
+    const newPlayer: Player = new Player(name, birthMonth, score);
+    this.playerService.addPlayer(newPlayer);
+  }
 }
